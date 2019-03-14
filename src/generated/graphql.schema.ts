4ecfb89aa6c8392da37311b0e347b812373619f0
Ruslan Shashkov <rshashkov@icloud.com>
2019-03-14 19:42:11 +0200

    sd

--- src/generated/graphql.schema.ts
+++ src/generated/graphql.schema.ts
@@ -1,102 +1,110 @@
 /* tslint:disable */
 export enum CommonOrderDirection {
-    ASC = "ASC",
-    DESC = "DESC"
+  ASC = 'ASC',
+  DESC = 'DESC',
 }
 
 export enum EProjectStatus {
-    Ready = "Ready",
-    TranslationInProgress = "TranslationInProgress",
-    Archive = "Archive"
+  Ready = 'Ready',
+  TranslationInProgress = 'TranslationInProgress',
+  Archive = 'Archive',
 }
 
 export enum EProjectType {
-    WebApplication = "WebApplication",
-    IOs = "IOs",
-    Android = "Android",
-    DesktopApplication = "DesktopApplication",
-    Promo = "Promo",
-    WebSite = "WebSite",
-    Api = "Api",
-    Other = "Other"
+  WebApplication = 'WebApplication',
+  IOs = 'IOs',
+  Android = 'Android',
+  DesktopApplication = 'DesktopApplication',
+  Promo = 'Promo',
+  WebSite = 'WebSite',
+  Api = 'Api',
+  Other = 'Other',
 }
 
 export enum PhraseOrderBy {
-    phraseId = "phraseId",
-    id = "id"
+  phraseId = 'phraseId',
+  id = 'id',
 }
 
 export enum ProjectOrderBy {
-    id = "id"
+  id = 'id',
 }
 
 export class CreatePhraseInput {
-    projectId: number;
-    phraseId: string;
-    tags?: string[];
+  projectId: number;
+  phraseId: string;
+  tags?: string[];
 }
 
 export class CreateProjectInput {
-    title: string;
+  title: string;
 }
 
 export class GetPhrasesInput {
-    projectId: string;
-    skip: number;
-    take: number;
-    orderBy: PhraseOrderBy;
-    orderDirection: CommonOrderDirection;
+  projectId: string;
+  skip: number;
+  take: number;
+  orderBy: PhraseOrderBy;
+  orderDirection: CommonOrderDirection;
 }
 
 export class GetProjectsInput {
-    skip: number;
-    take: number;
-    orderBy: ProjectOrderBy;
-    orderDirection: CommonOrderDirection;
+  skip: number;
+  take: number;
+  orderBy: ProjectOrderBy;
+  orderDirection: CommonOrderDirection;
 }
 
 export abstract class IMutation {
-    abstract createPhrase(createPhraseInput?: CreatePhraseInput): Phrase | Promise<Phrase>;
+  abstract createPhrase(
+    createPhraseInput?: CreatePhraseInput,
+  ): Phrase | Promise<Phrase>;
 
-    abstract createProject(createProjectInput?: CreateProjectInput): Project | Promise<Project>;
+  abstract createProject(
+    createProjectInput?: CreateProjectInput,
+  ): Project | Promise<Project>;
 }
 
 export class Phrase {
-    id: number;
-    phraseId: string;
-    tags?: string[];
+  id: number;
+  phraseId: string;
+  tags?: string[];
 }
 
 export class Project {
-    id: number;
-    title: string;
-    description: string;
-    type: EProjectType;
-    status: EProjectStatus;
-    lastEdit: Date;
-    readyness: number;
-    basePhrases: number;
-    baseWords: number;
-    issues: number;
-    avatar: string;
+  id: number;
+  title: string;
+  description: string;
+  type: EProjectType;
+  status: EProjectStatus;
+  lastEdit: Date;
+  readyness: number;
+  basePhrases: number;
+  baseWords: number;
+  issues: number;
+  avatar: string;
 }
 
 export abstract class IQuery {
-    abstract getPhrases(getPhrasesInput?: GetPhrasesInput): Phrase[] | Promise<Phrase[]>;
+  abstract getPhrases(
+    getPhrasesInput?: GetPhrasesInput,
+  ): Phrase[] | Promise<Phrase[]>;
 
-    abstract getPhrase(id: string): Phrase | Promise<Phrase>;
+  abstract getPhrase(id: string): Phrase | Promise<Phrase>;
 
-    abstract getProjects(getProjectsInput?: GetProjectsInput): Project[] | Promise<Project[]>;
+  abstract getProjects(
+    getProjectsInput?: GetProjectsInput,
+  ): Project[] | Promise<Project[]>;
 
-    abstract getProject(id: string): Project | Promise<Project>;
+  abstract getProject(id: string): Project | Promise<Project>;
 
-    abstract temp__(): boolean | Promise<boolean>;
+  abstract temp__(): boolean | Promise<boolean>;
 }
 
 export abstract class ISubscription {
-    abstract phraseCreated(): Phrase | Promise<Phrase>;
+  abstract phraseCreated(): Phrase | Promise<Phrase>;
 
-    abstract projectCreated(): Project | Promise<Project>;
+  abstract projectCreated(): Project | Promise<Project>;
 }
 
 export type Date = any;
--- src/managers/PhrasesManager.ts
+++ src/managers/PhrasesManager.ts
@@ -1,53 +1,57 @@
-import { CommonOrderDirection, GetPhrasesInput, Phrase, PhraseOrderBy, Project } from '../generated/graphql.schema';
+import {
+  CommonOrderDirection,
+  GetPhrasesInput,
+  Phrase,
+  PhraseOrderBy,
+  Project,
+} from '../generated/graphql.schema';
 import { apolloClient } from '../api/network.layer';
 import { gql } from 'apollo-boost';
 
 export class PhrasesManager {
-	public reset(): void {
+  public reset(): void {}
 
-	}
+  public init(): Promise<any> {
+    return Promise.resolve();
+  }
 
-	public init(): Promise<any> {
-		return Promise.resolve();
-	}
+  public async getPhrases(projectId: string): Promise<Phrase[]> {
+    const result = await apolloClient.query<
+      { getPhrases: Phrase[] },
+      GetPhrasesInput
+    >({
+      query: gql`
+        query(
+          $projectId: ID!
+          $skip: Int!
+          $take: Int!
+          $orderBy: PhraseOrderBy!
+          $orderDirection: CommonOrderDirection!
+        ) {
+          getPhrases(
+            getPhrasesInput: {
+              projectId: $projectId
+              skip: $skip
+              take: $take
+              orderBy: $orderBy
+              orderDirection: $orderDirection
+            }
+          ) {
+            id
+            phraseId
+            tags
+          }
+        }
+      `,
+      variables: {
+        projectId,
+        skip: 0,
+        take: 1000,
+        orderBy: PhraseOrderBy.id,
+        orderDirection: CommonOrderDirection.DESC,
+      },
+    });
 
-	public async getPhrases(projectId: string): Promise<Phrase[]> {
-		const result = await apolloClient.query<
-			{ getPhrases: Phrase[] },
-			GetPhrasesInput
-			>({
-			query: gql`
-				query(
-				$projectId: ID!
-				$skip: Int!
-				$take: Int!
-				$orderBy: PhraseOrderBy!
-				$orderDirection: CommonOrderDirection!
-				) {
-					getPhrases(
-						getPhrasesInput: {
-							projectId: $projectId
-							skip: $skip
-							take: $take
-							orderBy: $orderBy
-							orderDirection: $orderDirection
-						}
-					) {
-						id
-						phraseId
-						tags
-					}
-				}
-			`,
-			variables: {
-				projectId,
-				skip: 0,
-				take: 1000,
-				orderBy: PhraseOrderBy.id,
-				orderDirection: CommonOrderDirection.DESC,
-			},
-		});
-
-		return result.data.getPhrases;
-	}
+    return result.data.getPhrases;
+  }
 }
